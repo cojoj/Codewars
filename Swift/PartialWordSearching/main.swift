6a3bdcd2d0cc5e9d84b7412e40e24c6882db7d50
Mateusz Zając <zajac.mateusz@icloud.com>
2017-06-23 09:50:02 +0200

    Partial word searching kata

---
+++ Swift/PartialWordSearching/main.swift
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/54b81566cd7f51408300022d
+
+func wordSearch(_ str:String, _ arr:[String]) -> [String] {
+	let filtered = arr.filter { string in
+		string.lowercased().contains(str.lowercased())
+	}
+	
+	return filtered.count > 0 ? filtered : ["Empty"]
+}
