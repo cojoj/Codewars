9b3076a18d38fa9822de0fb1720b4aaf614e5c66
Mateusz Zając <zajac.mateusz@icloud.com>
2017-06-26 13:45:42 +0200

    Sum without highest and lowest number in Haskell

---
+++ Haskell/Codewars.hsproj/BoundlessSum/Sum.hs
@@ -0,0 +1,13 @@
+module BoundlessSum.Sum where
+    
+-- https://www.codewars.com/kata/sum-without-highest-and-lowest-number
+    
+import Data.List
+    
+sumArray :: Maybe [Int] -> Int
+sumArray Nothing            = 0
+sumArray (Just [])          = 0
+sumArray (Just (x:[]))      = 0
+sumArray (Just (x:y:[]))    = 0
+sumArray (Just x)           = sum bounded
+                                where bounded = tail $ init $ sort x
