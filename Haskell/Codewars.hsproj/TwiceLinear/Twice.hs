77b2974bde971f648a69c3628aab0a3e9e5d13cd
Mateusz Zając <zajac.mateusz@icloud.com>
2017-06-28 13:12:26 +0200

    Twice Linear in Haskell

---
+++ Haskell/Codewars.hsproj/TwiceLinear/Twice.hs
@@ -0,0 +1,13 @@
+module TwiceLinear.Twice where
+    
+-- https://www.codewars.com/kata/twice-linear
+    
+import Data.List
+import qualified Data.Set as S
+
+generateSeq :: [Integer]
+generateSeq = unfoldr (fmap foo . S.minView) $ S.singleton 1 
+    where foo (x, s) = (x, S.insert (2*x+1) $ S.insert (3*x+1) s)
+
+dblLinear :: Int -> Integer
+dblLinear n = generateSeq !! n
